ca0995e3ec179ede5d370e49363cb801a19402e2
yomidigital <dev@yomi.digital>
2024-04-24 11:17:49 +0200

    time format

--- src/screens/feed/FeedScreen.tsx
+++ src/screens/feed/FeedScreen.tsx
@@ -121,6 +121,31 @@ function FeedScreen({navigation}: HomeTabScreenProps<'Feed'>) {
     fetchFeed();
   }, [authContext]);
 
+  function formatDate(date: Date): string {
+    const now = new Date();
+    const diffInMilliseconds = now.getTime() - date.getTime();
+    const diffInMinutes = diffInMilliseconds / (1000 * 60);
+    const diffInHours = diffInMilliseconds / (1000 * 60 * 60);
+    const diffInDays = diffInMilliseconds / (1000 * 60 * 60 * 24);
+
+    if (diffInMinutes < 1) {
+      return 'just now';
+    } else if (diffInHours < 1) {
+      return `${Math.floor(diffInMinutes)}m`;
+    } else if (diffInHours < 24) {
+      return `${Math.floor(diffInHours)}h`;
+    } else if (diffInDays < 3) {
+      return `${Math.floor(diffInDays)}d`;
+    } else {
+      const day = date.getDate();
+      const month = date.getMonth() + 1; // Months are zero based
+      const year = date.getFullYear();
+      return `${month < 10 ? '0' + month : month}-${
+        day < 10 ? '0' + day : day
+      }-${year}`;
+    }
+  }
+
   return (
     <View>
       {feedFetchStatus === 'success' ? (
@@ -154,7 +179,7 @@ function FeedScreen({navigation}: HomeTabScreenProps<'Feed'>) {
                 headerSubtitle = '@' + item.author.username;
               }
 
-              const postTime = new Date(item.timestamp).toUTCString();
+              const postTime = formatDate(new Date(item.timestamp));
               let embed = item.embeds.find(
                 el => el.url !== '' && el.url !== null && el.url !== undefined,
               );
